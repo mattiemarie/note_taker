21e2aab2c65fe58600928cf81241f128bfefdd69
Mattie Gonzalez <gonzalem@lssc.edu>
2022-12-17 11:37:01 -0500

    Adding to server.js

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const path = require('path');
+
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Middleware for parsing JSON and urlencoded from data
+app.use(express.json());
+app.use(express.urlencoded({ extended:true}));
+app.use('/api', api);
+
+app.use(express.static('public'));
+
+// GET Route for Home Page
+app.get('/', (req,res) =>
+    res.sendFile(path.join(_dirname, './public/index.html'))
+);
+
+// GET Route for Notes Page
+app.get('/notes', (req, res) =>
+    res.sendFile(path.join(_dirname, './public/notes.html'))
+);
+
+app.listen(PORT, () => 
+    console.log(`App listening at http://localhost:${PORT}`)
+);
