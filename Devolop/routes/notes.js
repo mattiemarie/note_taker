55810767584326f590a267d0215661dcb977d6d0
Mattie Gonzalez <gonzalem@lssc.edu>
2022-12-18 12:16:30 -0500

    Adding to notes.js

--- Devolop/routes/notes.js
+++ Devolop/routes/notes.js
@@ -1,22 +1,41 @@
 const notes = require('express').Router();
-const { readFromFile, readAndAppend } = require ();
+const { readFromFile, readAndAppend } = require ('../helpers/fs.Utilis');
 
 
 // GET Route for retrieving all the tips
-tips.get('/', (req,res) => {
+notes.get('/', (req,res) => {
     readFromFile('./db/db.json').then((data) =>
     res.json(JSON.parse(data)));
 });
 
+// GET Route for retrieving all notes
+notes.get('/', (res, res) => {
+    readFromFile('../db/db.json').then((data) =>res.json(JSON.parse(data)));
+});
+
+// GET Route for a specific note
+notes.get('/:note_id', (req, res) => {
+    const noteId = req.params.note_id;
+    readFromFile('./db/db.json')
+        .then((data) => JSON.parse(data))
+        .then((json) => {
+            const result = json.filter((note) => note.note_id === noteId);
+            return result.length > 0
+                ? res.json(result)
+                : res.json ('No Note with that ID');
+        });
+});
+
 //POST Route for a new Note
-tips.post('/', (req,res) => {
+notes.post('/', (req, res) => {
     console.log(req.body);
 
-    const { username, topic, tip } = req.body;
+    const { createNewNote, deleteNewNote } = req.body;
 
     if(req.body) {
         const newNote = {
-            note
+            createNewNote,
+            deleteNewNote
         };
 
         readAndAppend(newNote, './db/db.json');
