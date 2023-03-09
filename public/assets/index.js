147e8bbfc04a8c486f08af1004b5683073e92cab
Mattie Gonzalez <gonzalem@lssc.edu>
2023-03-09 09:25:58 -0500

    modifing server.js

--- public/assets/index.js
+++ public/assets/index.js
@@ -4,7 +4,7 @@ let saveNoteBtn;
 let newNoteBtn;
 let noteList;
 
-if (window.location.pathname === './notes') {
+if (window.location.pathname === '/notes') {
   noteTitle = document.querySelector('.note-title');
   noteText = document.querySelector('.note-textarea');
   saveNoteBtn = document.querySelector('.save-note');
@@ -119,6 +119,7 @@ const handleRenderSaveBtn = () => {
 // Render the list of note titles
 const renderNoteList = async (notes) => {
   let jsonNotes = await notes.json();
+  console.log(jsonNotes)
   if (window.location.pathname === '/notes') {
     noteList.forEach((el) => (el.innerHTML = ''));
   }
--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const fs = require('fs');
+// const fs = require('fs');
 const path = require('path');
-const util = require('util');
-const { v4: uuidv4 } = require('uuid')
+
+const apiRoutes = require('./routes/notes');
+
 
 //Setting up Server
 const PORT = process.env.PORT || 3001;
@@ -12,61 +13,26 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended:true}));
 
-app.use(express.static('./develop/public'));
+app.use(express.static(path.join(__dirname, "public")));
+app.use('/api', apiRoutes);
 
-// Promise version of Fs.Readfile (mini project)
-const readFromFile = util.promisify(fs.readFromFile);
-const writeToFile = util.promisify(fs.writeToFile);
-
-// GET request | API Route
-app.get('/api/notes', function(req,res) {
-    readFromFile('./develop/db/db.json', 'utf8').then(function(data) {
-        notes = [].concat(JSON.parse(data))
-        res.json(notes)
-    })
-});
-
-// POST request | API Route
-app.post('/api/notes' , function(req, res) {
-    const note = req.body;
-    readFromFile('./develop/db/db.json', 'utf-8').then(function(data) {
-        const notes = [].concat(JSON.parse(data));
-        note.id = notes.length + 1
-        notes.push(note);
-        return notes
-    }).then(function(notes) {
-        writeFile('./develop/db/db.json', JSON.stringify(notes))
-        res.json(note);
-    })
-});
-
-// DELETE request | API Route
-app.delete('/api/notes/:id', function(req,res) {
-    const deletedNote = parseInt(req.params.id);
-    readFile('./develop/db/db.json', 'utf8').then(function(data) {
-        const notes = [].concat(JSON.parse(data));
-        const newNote = []
-        for(let i = 0; i < notes.length; i++) {
-            if(deletedNote !== notes[i].id)
-                newNote.push(notes[i])
-        }
-    })
-});
 
 // GET Route for Home Page
-app.get('/', (req,res) =>
-    res.sendFile(path.join(_dirname, './develop/public/index.html'))
+app.get('*', (req,res) =>
+    res.sendFile(path.join(__dirname, './public/index.html'))
 );
 
 // GET Route for Notes Page
 app.get('/notes', (req, res) =>
-    res.sendFile(path.join(_dirname, './develop/public/notes.html'))
+    res.sendFile(path.join(__dirname, './public/notes.html'))
 );
 
-// GET Route to direct users to a 404 page
-app.get('*', (req, res) =>
-    res.sendFile(path.join(_dirname, 'public/pages/404.html'))
-);
+// GET `db.json` file/RETURN saved notes
+app.get('/api/notes',(req, res) => {
+    res.sendFile(path.join(__dirname, '/db/db.json'));
+});
+
+
 
 // Port is Listening
 app.listen(PORT, () => 
